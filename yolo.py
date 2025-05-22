47f18e255b783ae0cee3a1cc0b70420158842c79
aliyun0442448136 <50014026+liyinze1@users.noreply.github.com>
2025-05-22 15:29:25 +0200

    update model name

--- yolo.py
+++ yolo.py
@@ -2,7 +2,17 @@ import numpy as np
 from tflite_runtime.interpreter import Interpreter
 
 # Load the model
-interpreter = Interpreter(model_path='model.tflite')
+interpreter = Interpreter(model_path='models/yolo11n_float16.tflite')
+interpreter.allocate_tensors()
+
+# Get input and output details
+input_details = interpreter.get_input_details()
+output_details = interpreter.get_output_details()
+
+print("Input details:", input_details)
+print("Output details:", output_details)
+
+interpreter = Interpreter(model_path='models/yolo11n_float32.tflite')
 interpreter.allocate_tensors()
 
 # Get input and output details
