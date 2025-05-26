aadbc0b989bc897de516b8f7b226f23ef4b74cc5
aliyun0442448136 <50014026+liyinze1@users.noreply.github.com>
2025-05-26 16:09:58 +0200

    update coco

--- yolo_train.py
+++ yolo_train.py
@@ -6,4 +6,4 @@ model = YOLO("yolo11n.pt")  # load a pretrained model (recommended for training)
 # model = YOLO("yolo11n.yaml").load("yolo11n.pt")  # build from YAML and transfer weights
 
 # Train the model
-results = model.train(data="coco8.yaml", epochs=100, imgsz=640)
+results = model.train(data="coco.yaml", epochs=100, imgsz=640)
