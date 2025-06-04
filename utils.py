e965fd6cb14afa6d01304bdae95aefa8d4eeb75b
aliyun0442448136 <50014026+liyinze1@users.noreply.github.com>
2025-06-04 22:27:47 +0200

    update utils

---
+++ utils.py
@@ -0,0 +1,40 @@
+import mpio
+import serial
+
+class Flute_Connection:
+    def __init__(self):
+        
+        self.s1 = mpio.GPIO(11, mpio.GPIO.OUT) # PA11
+        self.s2 = mpio.GPIO(13, mpio.GPIO.OUT) # PA13
+        self.s3 = mpio.GPIO(17, mpio.GPIO.OUT) # PA17
+        
+        self.s1.set(True)
+        self.s2.set(True)
+        self.s3.set(True)
+    
+    def read_pv_voltage(self):
+        with open('/sys/bus/iio/devices/iio:device0/in_voltage0_raw') as f:
+            value=int(f.read())
+        return value
+    
+    def read_cap_voltage(self):
+        with open('/sys/bus/iio/devices/iio:device0/in_voltage1_raw') as f:
+            value=int(f.read())
+        return value
+    
+
+class Modem_Connection:
+    def __init__(self):
+        
+        
+        pass
+    
+class RoadRunner:
+    
+    @staticmethod
+    def suspend_to_ram():
+        pass
+    
+    @staticmethod
+    def shutdown():
+        pass
