4ccad585a1964089dee1c33d03191a6e773ed26a
aliyun0442448136 <50014026+liyinze1@users.noreply.github.com>
2025-09-15 16:50:39 +0200

    rename export

--- yolo_export.py
+++ yolo_export.py
@@ -5,4 +5,4 @@ from ultralytics import YOLO
 model = YOLO('models/best.pt')  # load a pretrained model (recommended for training)
 # model = YOLO("yolo11n.yaml").load("yolo11n.pt")  # build from YAML and transfer weights
 
-model.export(format='tflite')
+model.export(format='tflite', imgsz=640)  # export the model to TensorFlow Lite format
