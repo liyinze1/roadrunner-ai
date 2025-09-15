9ab47ba8dfec5045991f4a0958e9dbc33dba4ed0
aliyun0442448136 <50014026+liyinze1@users.noreply.github.com>
2025-09-15 16:25:50 +0200

    new model

--- yolo_train.py
+++ yolo_train.py
@@ -2,10 +2,7 @@ from ultralytics import YOLO
 
 # Load a model
 # model = YOLO("yolo11n.yaml")  # build a new model from YAML
-model = YOLO("yolo11n.pt")  # load a pretrained model (recommended for training)
+model = YOLO('models/best.pt')  # load a pretrained model (recommended for training)
 # model = YOLO("yolo11n.yaml").load("yolo11n.pt")  # build from YAML and transfer weights
 
-# Train the model
-results = model.train(data="coco.yaml", epochs=100, imgsz=640)
-
-model.export(format="tflite")
+model.export(format='tflite')
