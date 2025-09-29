1876ba0a2b59be55b9dde05cd61f05b200559148
aliyun0442448136 <50014026+liyinze1@users.noreply.github.com>
2025-09-29 15:37:41 +0200

    update uart

--- uart.py
+++ uart.py
@@ -5,10 +5,10 @@ class uart_connection:
     def __init__(self, uart='/dev/ttyS1', baudrate=230400, timeout=0.1):
         self.uart = Serial(uart, baudrate)
         
+        self.stop_recieve = threading.Event()
+        self.ack_received = threading.Event()
         self.receive_thread = threading.Thread(target=self.receive)
         self.receive_thread.start()
-        self.ack_received = threading.Event()
-        self.stop_recive = threading.Event()
         self.timeout = timeout
                 
     def send_depth(self, depth):
@@ -18,7 +18,7 @@ class uart_connection:
         return self._send(b'P' + int.to_bytes(len(data), 2, 'big') + data)
     
     def reqest_sleep(self):
-        self.stop_recive.set()
+        self.stop_recieve.set()
         self.receive_thread.join()
         self._send(b'S')
         sleep_mode = self.uart.read(1)
@@ -34,7 +34,7 @@ class uart_connection:
             return False
         
     def receive(self):
-        while self.stop_recive.is_set() is False:
+        while self.stop_recieve.is_set() is False:
             c = self.uart.read(1)
             print('uart get', c)
             if c == b'A':
