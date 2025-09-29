6dd9fa8ec69e3bfe147f331f8cf87794078b68d2
aliyun0442448136 <50014026+liyinze1@users.noreply.github.com>
2025-09-29 15:31:02 +0200

    update uart

--- uart.py
+++ uart.py
@@ -5,7 +5,6 @@ class uart_connection:
     def __init__(self, uart='/dev/ttyS1', baudrate=230400, timeout=0.1):
         self.uart = Serial(uart, baudrate)
         
-        self.receive = True
         self.receive_thread = threading.Thread(target=self.receive)
         self.receive_thread.start()
         self.ack_received = threading.Event()
@@ -16,14 +15,6 @@ class uart_connection:
     
     def send_photo(self, data):
         return self._send(b'P' + int.to_bytes(len(data), 2, 'big') + data)
-    
-    def send_sleep_request(self):
-        self.receive = False
-        self.receive_thread.join()
-        self.uart.write(b'S')
-        sleep_mode = self.uart.read(1)
-        self.uart.close()
-        return sleep_mode
         
     def _send(self, data):
         self.uart.write(data)
@@ -35,14 +26,13 @@ class uart_connection:
             return False
         
     def receive(self):
-        while self.receive:
+        while True:
             c = self.uart.read(1)
             print('uart get', c)
             if c == b'A':
                 self.ack_received.set()
             elif c == b'N':
                 print('Error received!')
-                
                 
 if __name__ == '__main__':
     uart = uart_connection()
