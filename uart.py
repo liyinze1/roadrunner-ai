a5cd43a11d74f733e8f9eee10aab3ca293afa7ba
aliyun0442448136 <50014026+liyinze1@users.noreply.github.com>
2025-09-29 15:27:38 +0200

    update uart

--- uart.py
+++ uart.py
@@ -1,12 +1,11 @@
 import threading
-import time
 from serial import Serial
 
 class uart_connection:
     def __init__(self, uart='/dev/ttyS1', baudrate=230400, timeout=0.1):
         self.uart = Serial(uart, baudrate)
         
-    
+        self.receive = True
         self.receive_thread = threading.Thread(target=self.receive)
         self.receive_thread.start()
         self.ack_received = threading.Event()
@@ -14,23 +13,35 @@ class uart_connection:
                 
     def send_depth(self, depth):
         return self._send(b'D' + int.to_bytes(2, 2, 'big') + int.to_bytes(depth, 2, 'big'))
+    
+    def send_photo(self, data):
+        return self._send(b'P' + int.to_bytes(len(data), 2, 'big') + data)
+    
+    def send_sleep_request(self):
+        self.receive = False
+        self.receive_thread.join()
+        self.uart.write(b'S')
+        sleep_mode = self.uart.read(1)
+        self.uart.close()
+        return sleep_mode
         
     def _send(self, data):
         self.uart.write(data)
         if self.ack_received.wait(timeout=self.timeout):
             self.ack_received.clear()
-            print('Ack received')
             return True
         else:
             print('Timeout waiting for ack')
             return False
         
     def receive(self):
-        while True:
+        while self.receive:
             c = self.uart.read(1)
             print('uart get', c)
             if c == b'A':
                 self.ack_received.set()
+            elif c == b'N':
+                print('Error received!')
                 
                 
 if __name__ == '__main__':
