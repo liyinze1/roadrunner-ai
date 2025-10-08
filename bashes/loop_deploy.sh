c4b65ec6b763c7691b342b88aa14194ca4b9d27f
aliyun0442448136 <50014026+liyinze1@users.noreply.github.com>
2025-10-08 15:22:13 +0200

    update image path

--- bashes/loop_deploy.sh
+++ bashes/loop_deploy.sh
@@ -17,7 +17,7 @@ while true; do
 
     filename="/home/acme/roadrunner-ai/photos/$(date +%Y%m%d_%H%M%S).png"
     # sudo fswebcam -i 0 -p RGB565 -r 1280x720 -S 20 --no-banner "$filename"
-    sudo fswebcam -i 0 -p RGB565 -r 1920x1080 -S 20 --no-banner "$filename"
+    sudo fswebcam -i 0 -p RGB565 -r 1920x1080 -S 20 --no-banner $filename
 
 
     echo "$(date '+%Y-%m-%d %H:%M:%S') - inference..." >> log.txt
