e630e06c7315d7db4fb404c6beab2df9e2fe8d2a
aliyun0442448136 <50014026+liyinze1@users.noreply.github.com>
2025-11-07 16:30:08 +0100

    new int 8 model

--- uart.py
+++ uart.py
@@ -5,7 +5,7 @@ import time
 
 
 class uart_connection:
-    def __init__(self, uart='/dev/ttyS1', baudrate=230400, timeout=0.1):
+    def __init__(self, uart='/dev/ttyS1', baudrate=115200, timeout=1):
         self.uart = Serial(uart, baudrate)
         
         self.stop_recieve = threading.Event()
@@ -14,29 +14,11 @@ class uart_connection:
         self.receive_thread.start()
         self.timeout = timeout
         
-        # self.wake_gpio()
-        
-    # def wake_gpio(self, gpio_pin=103):
-    #     chip = gpiod.Chip('gpiochip0')
-    #     line = chip.get_line(gpio_pin)
-    #     line.request(consumer='my-app', type=gpiod.LINE_REQ_DIR_OUT, default_vals=[0])
-        
-    #     while True:
-    #         line.set_value(0)
-    #         time.sleep(0.1)
-    #         line.set_value(1)
-    #         if self.ack_received.wait(timeout=1):
-    #             self.ack_received.clear()
-    #             print('Wake up success')
-    #             break
-    #     line.release()
                 
     def send_depth(self, depth):
         return self._send(b'D' + int.to_bytes(2, 2, 'big') + int.to_bytes(depth, 2, 'big'))
     
     def send_photo(self, data):
-        print('Sending photo chunk of size', len(data))
-        print(int.to_bytes(len(data), 2, 'big'))
         return self._send(b'P' + int.to_bytes(len(data), 2, 'big') + data)
     
     def reqest_sleep(self):
@@ -52,14 +34,15 @@ class uart_connection:
             # power off
             return 0
         
-    def _send(self, data):
-        self.uart.write(data)
-        if self.ack_received.wait(timeout=self.timeout):
-            self.ack_received.clear()
-            return True
-        else:
-            print('Timeout waiting for ack')
-            return False
+    def _send(self, data, resend=5):
+        assert resend >= 0
+        for _ in range(resend + 1):
+            self.uart.write(data)
+            if self.ack_received.wait(timeout=self.timeout):
+                self.ack_received.clear()
+                return True
+        print('Timeout waiting for ack after', resend + 1, 'tries')
+        return False
         
     def receive(self):
         while self.stop_recieve.is_set() is False:
--- water_inference.py
+++ water_inference.py
@@ -340,7 +340,7 @@ def main():
     
     t = time.time()
     
-    model_path = "models/water_float32.tflite"
+    model_path = "models/water_int8.tflite"
     yolo = YOLOv11Segmentation(model_path, conf_threshold=0.5)
     
     # Run inference
